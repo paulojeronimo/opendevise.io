4a7e2b5765ceb5dce5df5d526045e36570420e6e
Dan Allen <dan.j.allen@gmail.com>
2015-10-29 17:44:14 -0600

    optimize JavaScript; make fixes for IE9

--- source/js/site.js
+++ source/js/site.js
@@ -8,54 +8,29 @@ $(document).foundation();
  * Adapted from cbpAnimatedHeader.js v1.0.0
  * Copyright 2013, Codrops - http://www.codrops.com - MIT licensed
  */
-(function() {
-  var navbar = document.querySelector('.global-nav'), changeAt = 50;
-
-  function init() {
-    window.addEventListener('scroll', scrollPage, false);
+!function(navSelector, changeAt) {
+  function onScroll() {
+    $(navSelector)[$(window).scrollTop() >= changeAt ? 'addClass' : 'removeClass']('navbar-shrink');
   }
+  window.addEventListener('scroll', onScroll, false);
+}('header.global-nav', 50);
 
-  function scrollPage() {
-    if (scrollY() >= changeAt) {
-      if (!navbar.classList.contains('navbar-shrink')) {
-        navbar.classList.add('navbar-shrink');
-      }
+!function(offset) {
+  function onScroll() {
+    var yScroll = $(window).scrollTop() + offset;
+    var currentEl = false;
+    $('[data-nav-dest]').each(function(idx) {
+      if (yScroll > $(this).offset().top) currentEl = this;
+    });
+    if (currentEl) {
+      var dest = currentEl.getAttribute('data-nav-dest');
+      $('[data-nav-href]').each(function(idx) {
+        $(this)[this.getAttribute('data-nav-href') === dest ? 'addClass' : 'removeClass']('active');
+      });
     }
     else {
-      navbar.classList.remove('navbar-shrink');
+      $('[data-nav-href]').removeClass('active');
     }
   }
-
-  function scrollY() {
-    return window.pageYOffset || document.documentElement.scrollTop;
-  }
-
-  init();
-})();
-
-function activateNavItem(e) {
-  var yScroll = window.pageYOffset + 50;
-  var currentEl = null;
-  $('[data-nav-dest]').each(function(idx, el) {
-    if (yScroll > $(el).offset().top) {
-      currentEl = el;
-    }
-  });
-  if (currentEl) {
-    $('[data-nav-href]').each(function(idx, el) {
-      if (currentEl.getAttribute('data-nav-dest') === el.getAttribute('data-nav-href')) {
-        if (!el.classList.contains('active')) el.classList.add('active');
-      }
-      else {
-        el.classList.remove('active');
-      }
-    });
-  }
-  else {
-    $('[data-nav-href]').each(function(idx, el) {
-      el.classList.remove('active');
-    });
-  }
-}
-
-window.addEventListener('scroll', activateNavItem);
+  window.addEventListener('scroll', onScroll, false);
+}(50);
